08d458278df39bb703d5090ddb48e5db44c3162b
maitrakesparkbiz <nirmal.maitrak@esparkbizmail.com>
2022-12-22 11:54:58 +0530

    init

---
+++ src/components/products/listproduct.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class ProductList extends Component{
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Product List</h1>
+            </div>)
+        }
+}
+
+export default ProductList;
